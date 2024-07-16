5b499ef4ebeea56c31413fe2ff4952ac8fbca0b0
Feng <63721131+lancelotzhang0124@users.noreply.github.com>
2024-07-16 15:05:19 +0800

    7.0. singleR.R

--- 7. annotation.R
+++ 7. annotation.R
@@ -1,15 +1,13 @@
 
-###################SingleR自动注释细胞###################
+###################SingleR annotation celltypes###################
 library(SingleR)
 library(celldex)
-#获取数据库
-hpca.se <- HumanPrimaryCellAtlasData()#这里会自动创建一个文档.cache/R/ExperimentHub
+#obtain annotation data base
+hpca.se <- HumanPrimaryCellAtlasData()#create file .cache/R/ExperimentHub
 hpca.se
-#获取标准化矩阵
 Standard_matrix <- GetAssayData(Seurat_object, slot="data")
-#定义clusters变量
 clusters=Seurat_object@meta.data$seurat_clusters
-#注释细胞
+#annotation
 pred.hpca.se <- SingleR(test = Standard_matrix, ref = hpca.se, labels = hpca.se$label.main,
                               method = "cluster", clusters = clusters,
                               assay.type.test = "logcounts",
