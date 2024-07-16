21f7086e46249431d38ecf18f8bed15a9ab31486
Feng <63721131+lancelotzhang0124@users.noreply.github.com>
2024-07-16 14:59:52 +0800

    Delete 5. Standard_Process.R

--- 5. Standard_Process.R
+++
@@ -1,9 +0,0 @@
-###########################标准化#####################
-Seurat_object <- NormalizeData(Seurat_object, normalization.method = "LogNormalize", scale.factor = 10000)
-###########################高变基因#####################
-Seurat_object <- FindVariableFeatures(Seurat_object, 
-                                      selection.method = "vst", 
-                                      nfeatures = 2000)
-##########################归一化#####################
-Seurat_object <- ScaleData(Seurat_object, features = rownames(Seurat_object))
-#以上每一步都需要大量运算能力，个人电脑慎运行
